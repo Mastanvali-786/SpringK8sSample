402a3eeb20ab97b04d1bee92a7e78ceecee1ace4
mastanvali2600 <31236620+mastanvali2600@users.noreply.github.com>
2025-05-09 20:40:52 +0530

    Initial commit

---
+++ src/main/java/com/mastan/k8s/SpringK8sSample/SpringK8sSampleApplication.java
@@ -0,0 +1,13 @@
+package com.mastan.k8s.SpringK8sSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringK8sSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringK8sSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mastan/k8s/SpringK8sSample/SpringK8sSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastan.k8s.SpringK8sSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringK8sSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
